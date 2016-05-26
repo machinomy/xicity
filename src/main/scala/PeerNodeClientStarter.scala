50d42d80f549cb35e11e8cef0124d344d3c98e71
Sergey Ukustov <sergey@ukstv.me>
2016-05-26 10:30:12 +0300

    peerNode

--- src/main/scala/PeerNodeClientStarter.scala
+++ src/main/scala/PeerNodeClientStarter.scala
@@ -11,7 +11,7 @@ object PeerNodeClientStarter {
     val system = ActorSystem()
     val identifier = new Identifier(34)
     val peerNode = system.actorOf(PeerNode.props(identifier))
-    val seeds = Set(Connector("localhost"))
+    val seeds = Set(Connector("45.55.122.116"))
     peerNode ! PeerNode.StartClientsCommand(2, seeds)
   }
 
@@ -22,7 +22,7 @@ object PeerNodeClientStarter {
     val system = ActorSystem()
     val identifier = Identifier(100)
     val peerNode = system.actorOf(PeerNode.props(identifier))
-    val seeds = Set(Connector("localhost"))
+    val seeds = Set(Connector("45.55.122.116"))
     peerNode ! PeerNode.StartClientsCommand(2, seeds)
     val cmd = PeerNode.SendSingleMessageCommand(identifier, new Identifier(BigInt(34)), "foo".getBytes, DateTime.now.getMillis / 1000 + 5)
     peerNode ! cmd
--- src/main/scala/PeerNodeStarter.scala
+++ src/main/scala/PeerNodeStarter.scala
@@ -1,10 +1,11 @@
-import akka.actor.ActorSystem
-import com.machinomy.xicity.{Connector, Identifier, PeerNode}
-
 object PeerNodeStarter {
   def main() = {
+    import akka.actor.ActorSystem
+    import com.machinomy.xicity.{Connector, Identifier, PeerNode}
     val system = ActorSystem()
     val peerNode = system.actorOf(PeerNode.props(Identifier.random))
-    peerNode ! PeerNode.StartServerCommand(Connector("localhost"))
+    peerNode ! PeerNode.StartServerCommand(Connector("0.0.0.0"))
+    val seeds = Set(Connector("45.55.122.116"))
+    peerNode ! PeerNode.StartClientsCommand(2, seeds)
   }
 }
