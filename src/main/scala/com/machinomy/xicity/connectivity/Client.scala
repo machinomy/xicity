906012d54fdc6cea420a91dfc0f46e81da0465bc
Sergey Ukustov <sergey@ukstv.me>
2016-07-02 23:59:58 +0300

    Client+ClientMonitor

---
+++ src/main/scala/com/machinomy/xicity/connectivity/Client.scala
@@ -0,0 +1,41 @@
+package com.machinomy.xicity.connectivity
+
+import java.net.InetSocketAddress
+
+import akka.actor.{Actor, ActorLogging, Props}
+import akka.io.{IO, Tcp}
+
+class Client(address: Address, initialBehavior: Client.Behavior) extends Actor with ActorLogging {
+  var behavior = initialBehavior
+
+  override def preStart(): Unit = {
+    implicit val actorSystem = context.system
+    IO(Tcp) ! Tcp.Connect(address.address)
+    log.info(s"Connecting to $address")
+  }
+
+  override def receive = {
+    case Tcp.Connected(remoteAddress, localAddress) =>
+      val endpoint = Endpoint(address, sender())
+      behavior = behavior.didConnect(endpoint, remoteAddress, localAddress)
+    case Tcp.CommandFailed(cmd) =>
+      log.info(s"Command $cmd failed!")
+      behavior = behavior.didDisconnect()
+      context.stop(self)
+  }
+
+  override def postStop(): Unit = {
+    log.info("Shutting down the client...")
+    behavior.didClose()
+  }
+}
+
+object Client {
+  trait Behavior {
+    def didConnect(endpoint: Endpoint, remoteAddress: InetSocketAddress, localAddress: InetSocketAddress): Behavior
+    def didDisconnect(): Behavior
+    def didClose(): Behavior
+  }
+
+  def props(address: Address, behavior: Behavior) = Props(classOf[Client], address, behavior)
+}
---
+++ src/main/scala/com/machinomy/xicity/connectivity/ClientMonitor.scala
@@ -0,0 +1,38 @@
+package com.machinomy.xicity.connectivity
+
+import akka.actor.{Actor, ActorRef}
+
+import scala.annotation.tailrec
+import scala.collection.immutable.IndexedSeq
+import scala.util.Random
+
+class ClientMonitor(seeds: Set[Address], threshold: Byte, initialBehavior: ClientMonitor.Behavior) extends Actor {
+  assert(threshold >= 0)
+
+  var behavior = initialBehavior
+
+  override def preStart(): Unit = {
+    behavior = addClients(selectSeeds(threshold), behavior)
+  }
+
+  override def receive = {
+    case something => throw new IllegalArgumentException(s"Not planned to receive anything, got: $something")
+  }
+
+  def selectSeeds(n: Byte): IndexedSeq[Address] = Random.shuffle(seeds.toIndexedSeq).take(n)
+
+  @tailrec
+  final def addClients(addresses: Seq[Address], behavior: ClientMonitor.Behavior): ClientMonitor.Behavior = addresses match {
+    case address :: as =>
+      val actor = context.actorOf(Client.props(address, behavior.clientBehavior))
+      addClients(as, behavior.addClient(actor))
+    case Seq.empty => behavior
+  }
+}
+
+object ClientMonitor {
+  trait Behavior {
+    def addClient(actor: ActorRef): Behavior
+    def clientBehavior: Client.Behavior
+  }
+}
