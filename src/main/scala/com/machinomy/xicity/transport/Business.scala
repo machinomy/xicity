22d6580bdffc3734a2adb3acd7f731a3aab8b58b
Sergey Ukustov <sergey@ukstv.me>
2016-07-05 05:57:55 +0300

    - Business

--- src/main/scala/com/machinomy/xicity/transport/Business.scala
+++
@@ -1,8 +0,0 @@
-package com.machinomy.xicity.transport
-
-import com.machinomy.xicity.transport.Message.Shot
-
-trait Business {
-  def didConnect(): Unit = {}
-  def didRead(message: Shot): Unit = {}
-}
