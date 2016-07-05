08865e5dcf5b269db9c5c23ab360d9da169076fc
Sergey Ukustov <sergey@ukstv.me>
2016-07-05 06:05:02 +0300

    + OutgoingConnectionBehavior

---
+++ src/main/scala/com/machinomy/xicity/transport/OutgoingConnectionBehavior.scala
@@ -0,0 +1,19 @@
+package com.machinomy.xicity.transport
+
+class OutgoingConnectionBehavior extends Connection.Behavior {
+  var endpointOpt: Option[Endpoint] = None
+
+  override def handle: Handle = {
+    case Connection.DidConnect(endpoint) =>
+      log.info(s"Connected to $endpoint")
+      endpointOpt = Some(endpoint)
+    case Connection.DidDisconnect() =>
+      log.info(s"Disconnected...")
+      context.stop(self)
+    case Connection.DidClose() =>
+      log.info(s"Closed...")
+      context.stop(self)
+    case Connection.DidRead(bytes) =>
+      log.info(s"Received $bytes")
+  }
+}
