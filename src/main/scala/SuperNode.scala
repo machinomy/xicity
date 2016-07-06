00ed0c5438ba2609da3021d8b0a8b477e4876b0c
Sergey Ukustov <sergey@ukstv.me>
2016-07-06 04:23:49 +0300

    Run SuperNode

---
+++ src/main/scala/SuperNode.scala
@@ -0,0 +1,23 @@
+import akka.actor.ActorSystem
+import akka.util.Timeout
+import com.machinomy.xicity.Identifier
+import com.machinomy.xicity.transport.{Address, Node, Parameters, ServerNode}
+
+import scala.concurrent.duration.FiniteDuration
+
+object SuperNode extends App {
+  implicit val system = ActorSystem()
+  val identifier = Identifier.random
+  println(s"Using identifier: $identifier")
+  val nodeActor = system.actorOf(Node.props(identifier))
+  val parameters = new Parameters {
+    override def threshold: Byte = Parameters.default.threshold
+    override def tickInterval: FiniteDuration = Parameters.default.tickInterval
+    override def tickInitialDelay: FiniteDuration = Parameters.default.tickInitialDelay
+    override def serverAddress: Address = Address("45.55.122.116")
+    override def seeds: Set[Address] = Set.empty
+    override def timeout: Timeout = Parameters.default.timeout
+    override def port: Int = Parameters.default.port
+  }
+  val serverNodeActor = system.actorOf(ServerNode.props(Node.Wrap(nodeActor, parameters)))
+}
