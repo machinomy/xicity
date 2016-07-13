5fed0af24f2d3cd324a56a8b5d93e69fb99dce4c
Sergey Ukustov <sergey@ukstv.me>
2016-07-13 19:26:47 +0300

    Simplify Address

--- src/main/scala/com/machinomy/xicity/transport/Address.scala
+++ src/main/scala/com/machinomy/xicity/transport/Address.scala
@@ -7,17 +7,9 @@ case class Address(address: InetSocketAddress)
 object Address {
   val PORT = 4240
 
-  def apply(host: String): Address = {
-    apply(host, PORT)
-  }
+  def apply(host: String): Address = apply(host, PORT)
 
-  def apply(host: String, port: Int): Address = {
-    val address = new InetSocketAddress(host, port)
-    Address(address)
-  }
+  def apply(host: String, port: Int): Address = Address(new InetSocketAddress(host, port))
 
-  def apply(ipAddress: InetAddress, port: Int): Address = {
-    val address = new InetSocketAddress(ipAddress, port)
-    Address(address)
-  }
+  def apply(ipAddress: InetAddress, port: Int): Address = Address(new InetSocketAddress(ipAddress, port))
 }
