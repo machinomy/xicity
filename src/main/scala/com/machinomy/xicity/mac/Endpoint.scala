b3120095c349f72957a717d1e707a1b460a0c493
Sergey Ukustov <sergey@ukstv.me>
2016-07-15 23:31:11 +0300

    Simplified Endpoint.toString

--- src/main/scala/com/machinomy/xicity/mac/Endpoint.scala
+++ src/main/scala/com/machinomy/xicity/mac/Endpoint.scala
@@ -4,4 +4,6 @@ import akka.actor.ActorContext
 
 case class Endpoint(address: Address, wire: Wire) {
   def write[A <: Message.Message](message: A)(implicit context: ActorContext) = wire.write(Message.encode(message))
+
+  override def toString: String = s"Endpoint(${address.address})"
 }
