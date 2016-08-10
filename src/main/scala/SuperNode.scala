ae6fc86aef09048b45684fd3334c8eb35ae44041
Sergey Ukustov <sergey@ukstv.me>
2016-08-10 16:14:05 +0300

    Fix SuperNode

--- src/main/scala/SuperNode.scala
+++ src/main/scala/SuperNode.scala
@@ -1,23 +1,27 @@
-import akka.actor.ActorSystem
-import akka.util.Timeout
-import com.machinomy.xicity.Identifier
-import com.machinomy.xicity.transport.{Address, Node, Parameters, ServerNode}
+import java.net.InetAddress
 
-import scala.concurrent.duration.FiniteDuration
+import akka.actor.{Actor, ActorLogging, ActorSystem, Props}
+import com.machinomy.xicity.Identifier
+import com.machinomy.xicity.mac.Parameters
+import com.machinomy.xicity.network.{Peer, PeerBase, Server}
 
 object SuperNode extends App {
-  implicit val system = ActorSystem()
+  val system = ActorSystem("xicity")
   val identifier = Identifier.random
+
   println(s"Using identifier: $identifier")
-  val nodeActor = system.actorOf(Node.props(identifier))
-  val parameters = new Parameters {
-    override def threshold: Byte = Parameters.default.threshold
-    override def tickInterval: FiniteDuration = Parameters.default.tickInterval
-    override def tickInitialDelay: FiniteDuration = Parameters.default.tickInitialDelay
-    override def serverAddress: Address = Address("45.55.122.116")
-    override def seeds: Set[Address] = Set.empty
-    override def timeout: Timeout = Parameters.default.timeout
-    override def port: Int = Parameters.default.port
+
+  class Dummy extends Actor with ActorLogging {
+    override def receive: Receive = {
+      case Peer.IsReady() =>
+        log.info(s"Ready to transmit messages")
+    }
   }
-  val serverNodeActor = system.actorOf(ServerNode.props(Node.Wrap(nodeActor, parameters)))
+  val dummy = system.actorOf(Props(classOf[Dummy]), "dummy")
+  val serverParameters = Parameters.default.copy(
+    seeds = Set.empty,
+    bindAddress = Some(InetAddress.getByName("45.55.122.116"))
+  )
+  val peerProps = PeerBase.props[Server](identifier, dummy, serverParameters)
+  system.actorOf(peerProps, "peer")
 }
